e76dc7daa368ba4d6aa82efeb3c6043f32f1e887
Divya <divyapandey090901@gmail.com>
2024-06-14 22:42:45 +0530

    third update

--- Object_Class/Classes.js
+++ Object_Class/Classes.js
@@ -1,4 +1,4 @@
-/*//Prior to the introduction of ES6 classes, JavaScript commonly used constructor functions to mimic class-like behavior
+//Prior to the introduction of ES6 classes, JavaScript commonly used constructor functions to mimic class-like behavior
 function Person (name,age) { 
     this.name=name;
     this.age=age;
@@ -10,19 +10,30 @@ function Person (name,age) {
 let person1=new Person("Divya",23);
 console.log(person1.name);
 
-*/
-//ES6 Classes
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    
-    greet() {
-        return `Hello, my name is ${this.name} and I'm ${this.age} years old.`;
-    }
-}
 
-// Creating an instance of Person
-let person1 = new Person("John", 30);
-console.log(person1.greet()); // Output: Hello, my name is John and I'm 30 years old.
+//ES6 Classes
+//Certainly! In JavaScript, you can create objects using ES6 classes, which are essentially 
+//syntactical sugar over the prototype-based inheritance model. 
+//Here's an example of how you can create an object using a class:
+class Person {
+
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+  
+    introduce() {
+      console.log(`Hello, my name is ${this.name} and I'm ${this.age} years old.`);
+    }
+  }
+  
+  // Creating instances
+  let person1 = new Person("Alice", 30);
+  let person2 = new Person("Bob", 25);
+  
+  // Using the shared method
+  person1.introduce(); // Outputs: Hello, my name is Alice and I'm 30 years old.
+  person2.introduce(); // Outputs: Hello, my name is Bob and I'm 25 years old.
+  
+//Yes, you can use prototype and class together in JavaScript. In fact, when you define a class in JavaScript, 
+//behind the scenes, it's still using prototypes for inheritance.
--- Object_Class/Object.js
+++ Object_Class/Object.js
@@ -27,3 +27,28 @@ person2.job='Developer';
 
 console.log(person1.greet());
 console.log(person2.greet());
+
+// object with prototype 
+//In JavaScript, the prototype property is a fundamental concept related to 
+//object-oriented programming. Every JavaScript function has a prototype property,
+// which is empty by default. You can use this property to add properties and methods to all objects created with that function as a constructor.
+//Prototypes are used for inheritance in JavaScript,you can attach properties and methods to the prototype object, which will be shared among all instances created from that constructor or class.
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.greet = function() {
+  return 'Hello, my name is ' + this.name + ' and I am ' + this.age + ' years old.';
+};
+
+let person1 = new Person('Ankita', 26);
+let person2 = new Person('Nikhil', 24);
+let person3 = new Person('Divya', 23);
+let person4 = new Person('Shivangi', 23);
+
+
+console.log(person1.greet()); 
+console.log(person2.greet()); 
+console.log(person3.greet()); 
+console.log(person4.greet()); 
